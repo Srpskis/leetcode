35e990add01ad832ed6aef036876d0205f301ce2
Ana Taylor <taylorana@seattleu.edu>
2020-09-03 16:17:43 -0700

    add solution

---
+++ strings+arrays/max_profit.py
@@ -0,0 +1,43 @@
+"""
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+Example 2:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+"""
+
+
+def max_profit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    profit = 0
+    if len(prices) > 1:
+        min = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < min:
+                min = prices[i]
+            elif prices[i] - min > profit:
+                profit = prices[i] - min
+            else:
+                continue
+
+    return profit
+
+
+array = [7,1,5,3,6,4]
+print(max_profit(array))
